a21fc4c0690fa4cb10a4b03f358fb5630d2a568a
Révi Norbert <revinorbi2003@gmail.com>
2024-04-18 15:53:15 +0300

    Instaled router dom and created TodoList.

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './Navbar/Navbar.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TodoList from './Pages/TodoList'
 
 function App() {
   return (
-      <Navbar />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<TodoList />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
   )
 }
 
